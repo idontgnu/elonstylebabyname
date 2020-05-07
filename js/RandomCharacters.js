2df3f7a27a05b45c8b4bad6c17e189e3257f6ac7
idontgnu <33888169+idontgnu@users.noreply.github.com>
2020-05-07 15:15:02 -0700

    Add files via upload

---
+++ js/RandomCharacters.js
@@ -0,0 +1,47 @@
+document.getElementById("generate-names").addEventListener('submit',generate);
+
+
+function generate_random_string(string_length){
+    var random_string = '';
+    var random_ascii;
+    var ascii_low = 32;
+    var ascii_high = 173;
+	var random_name = '';
+	
+    for(var i = 0; i < string_length; i++) {
+        random_ascii = Math.floor((Math.random() * (ascii_high - ascii_low)) + ascii_low);
+		 	 if (126 < random_ascii && random_ascii < 173){
+				random_ascii=32;
+				
+			}  
+			
+		random_string += String.fromCharCode(random_ascii)
+		console.log(random_ascii);
+	}
+	return random_string;
+    }
+		
+
+
+function generate_random_number(){
+    var num_low = 0;
+    var num_high = 9;
+    return Math.floor((Math.random() * (num_high - num_low)) + num_low);
+}
+
+function generate() {
+    return random_name = generate_random_string(generate_random_number()+1) + generate_random_number();
+	
+
+}
+
+console.log(generate());
+document.getElementById("generate-names").onclick = function(){
+var html='<h2>Your New Baby Name:</h2>';
+            html+='<ul class="list">';
+                html+=`
+                <li>${random_name}</li>
+                `;
+            html+='</ul>'
+            document.querySelector('#result').innerHTML=html;
+}
